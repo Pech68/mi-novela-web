6917cbc4f63fc68d786bb5f730c9dd2085d2f826
Pech68 <164519487+Pech68@users.noreply.github.com>
2025-09-22 17:32:33 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 module.exports = function(eleventyConfig) {
-  // Copiar la carpeta de assets (imágenes) y el CSS al sitio final
+  // Copiar la carpeta de assets, admin y el CSS al sitio final
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("style.css"); // <-- ESTA ES LA LÍNEA NUEVA Y CORREGIDA
+  eleventyConfig.addPassthroughCopy("style.css");
+
+  // ESTA ES LA PARTE NUEVA Y MÁS IMPORTANTE
+  // Crea una colección llamada "novelas" a partir de todos los archivos
+  // que tengan la etiqueta "novelas"
+  eleventyConfig.addCollection("novelas", function(collectionApi) {
+    return collectionApi.getFilteredByTag("novelas");
+  });
 
   return {
     // Directorios de entrada y salida
