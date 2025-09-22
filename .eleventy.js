d8f7dd263f216e48b8ffc1b5b75c072eabc8d5d3
Pech68 <164519487+Pech68@users.noreply.github.com>
2025-09-22 17:22:27 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
-  // Copiar la carpeta de assets (imágenes) al sitio final
+  // Copiar la carpeta de assets (imágenes) y el CSS al sitio final
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("style.css"); // <-- ESTA ES LA LÍNEA NUEVA Y CORREGIDA
 
   return {
     // Directorios de entrada y salida
