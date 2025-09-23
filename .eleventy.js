e643cd52ac3506c4e20a4102b51a1b652eb5fb87
Pech68 <164519487+Pech68@users.noreply.github.com>
2025-09-23 10:30:06 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,18 @@
 module.exports = function(eleventyConfig) {
+  // Copia los archivos y carpetas necesarios al sitio final
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("_includes");
+  eleventyConfig.addPassthroughCopy("manifest.json"); // <-- NUEVO
+  eleventyConfig.addPassthroughCopy("sw.js");           // <-- NUEVO
 
-  // Crea una colección para NOVELAS
+  // Crea una colección para las NOVELAS
   eleventyConfig.addCollection("novel", function(collectionApi) {
     return collectionApi.getFilteredByTag("novel");
   });
-
-  // Crea una colección para CAPÍTULOS
+  
+  // Crea una colección para los CAPÍTULOS
   eleventyConfig.addCollection("chapter", function(collectionApi) {
     return collectionApi.getFilteredByTag("chapter");
   });
