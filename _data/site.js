e8677527079ae0fd603bf15ef92cf5081c2542d3
Pech68 <164519487+Pech68@users.noreply.github.com>
2025-09-24 11:57:16 -0500

    Create site.js

---
+++ _data/site.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Esta línea es la importante. Obtiene la URL de tu sitio automáticamente desde Netlify.
+  url: process.env.URL || "http://localhost:8080",
+  name: "PechsNovel",
+  author: "Devinson Rodriguez"
+};
